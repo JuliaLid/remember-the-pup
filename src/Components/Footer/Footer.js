e8fc326fbeb19ae1bce67891d6da296a7b5e53d7
Julia Lidstrom <juleslidstrom@gmail.com>
2018-04-11 18:32:52 -0500

    Final app version

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,8 +3,8 @@ import "./Footer.css";
 
 const Footer = () =>(
  <footer className="footer">
-    <div className="bottom">
-         <a href="https://github.com/JuliaLid/remember-the-pup"><i class="fab fa-github"></i></a>
+    <div className="bottom text-center ">
+         <a href="https://github.com/JuliaLid/remember-the-pup"><i class="fab fa-github fa-3x"></i></a>
     </div>
 </footer>	
 
